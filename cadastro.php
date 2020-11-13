20e5565b69d3daaedac0ebc977785ba1a4d490ab
alcymotta <amottajunior@outlook.com>
2020-11-13 12:40:58 -0300

    Adicionado tela de registro e suas funções no source

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -11,28 +15,42 @@
         <div class="box">
             <div class="form">
                 <h2>Cadastrar</h2>
-                <form action="cadastrar.php" method="POST">
+                <form action="source/cadastrar.php" method="POST">
                     <div class="inputBx">
-                        <input type="text" placeholder="Nome">
+                        <input name="nome" type="text" placeholder="Nome">
                     </div>
                     <div class="inputBx">
-                        <input type="text" placeholder="Usuário">
+                        <input name="usuario" type="text" placeholder="Usuário">
                     </div>
                     <div class="inputBx">
-                        <input type="password" placeholder="Senha">
+                        <input name="senha" type="password" placeholder="Senha">
                     </div>
                     <div class="inputBx">
                         <input type="submit" value="Cadastrar">
                     </div>
                 </form>
-                <p>Já tenho uma conta, fazer <a href="#">login</a></p>
+                <p>Já tenho uma conta, fazer <a href="painel_login.php">login</a></p>
+                <?php
+                if(isset($_SESSION['status_cadastro'])):
+                ?>
                 <div class="is-success">
                     <p>Cadastro efetuado!</p>
-                    <p>Faça login informando o seu usuário e senha <a href="login.html">aqui<a/></p>
+                    <p>Faça login informando o seu usuário e senha <a href="painel_login.php">aqui</a></p>
                 </div>
+                <?php
+                endif;
+                unset($_SESSION['status_cadastro']);
+                ?>
+                <?php
+                if(isset($_SESSION['usuario_existe'])):
+                ?>
                 <div class="is-info">
                     <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
                 </div>
+                <?php
+                endif;
+                unset($_SESSION['usuario_existe']);
+                ?>
             </div>
         </div>
 
